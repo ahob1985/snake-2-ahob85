5d8753415fd2db6695088b58b901f5b2444acf23
ahob85 <aaron@ahob85.com>
2021-07-07 21:00:49 +0000

    updated project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Author:
+// Author: Aaron Hobson
 
 // Global UI Variables
 let canvasDiv;
@@ -46,7 +46,8 @@ function setup() {
   // Set the game's framerate to 5 (or whatever you prefer)
   frameRate(5);
   // Load the video
-
+  video = createCapture(VIDEO, videoReady);
+  video.parent(canvasDiv);
 }
 
 function draw() {
@@ -55,9 +56,10 @@ function draw() {
     if(knnClassifier.getNumLabels() > 0) {
       knnClassifier.classify(imgFeatures, gotResults);
       // Scale the canvas according to resolution, then refresh the background
-
+      scale(resolution);
+      background(220);
       // Draw game objects
-
+      drawGameObjects();
     }
   }
 }
@@ -70,7 +72,7 @@ function drawGameObjects() {
     textP.html("Score: " + score);
   };
   // Draw the snake, but first check the user's position
-
+  checkPosition();
   snake.update();
   snake.show();
   // Draw the food
@@ -93,13 +95,14 @@ function createFood() {
 }
 
 function checkPosition() {
-  if(keyCode === UP_ARROW && snake.yDirection === 0) {
+  let positionLabel = textP2.html().toLowerCase();
+  if(positionLabel.includes("up") && snake.yDirection === 0) {
     snake.setDirection(0, -1);
-  } else if(keyCode === DOWN_ARROW && snake.yDirection === 0) {
+  } else if(positionLabel.includes("down") && snake.yDirection === 0) {
     snake.setDirection(0, 1);
-  } else if(keyCode === LEFT_ARROW) && snake.xDirection === 0) {
+  } else if(positionLabel.includes("left") && snake.xDirection === 0) {
     snake.setDirection(-1, 0);
-  } else if(keyCode === RIGHT_ARROW && snake.xDirection === 0) {
+  } else if(positionLabel.includes("right") && snake.xDirection === 0) {
     snake.setDirection(1, 0);
   }
 }
@@ -123,7 +126,7 @@ function featureExtractorLoaded() {
   knnClassifier.load("model/myKNN.json", function () {
     isModelReady = true;
     // Reset the game
-
+    resetGame();
   });
 }
 
